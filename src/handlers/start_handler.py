3734520e8798217c7c9c3d607bf36e880ec76956
Kamidashi <lipatovb@gmail.com>
2025-05-11 22:34:06 +0300

    test workflow

--- src/handlers/start_handler.py
+++ src/handlers/start_handler.py
@@ -12,4 +12,4 @@ start_router = Router()
 async def start_message_handler(message: Message):
     kb = create_main_kb()
     await db.Database().register_user(message.from_user.id)
-    await message.answer(f'Привет!', reply_markup=kb)
+    await message.answer(f'Пока!', reply_markup=kb)
